5c7e62bfd6d7b0193af3b248d215b9fbce450c8d
Sebas <sebasgiustra@gmail.com>
2018-08-28 14:32:12 -0300

    primer push

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/reboot.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+
+</head>
+<body>
+	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-orange">
+		<div class="col-md-3">
+			<a class="navbar-brand" href="@">Eventos YA</a>
+		</div>
+		<div class="col-md-9">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle Navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Features</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Pricing</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#">Disabled</a>
+					</li>
+					<li class="nav-item dropdown">
+				        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				          Dropdown link
+				        </a>
+				        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				    	    <a class="dropdown-item" href="#">Action</a>
+				        	<a class="dropdown-item" href="#">Another action</a>
+				        	<a class="dropdown-item" href="#">Something else here</a>
+				        </div>
+				    </li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'header.php';
+
+
+?>
+<div class="container borde-azul">
+	div container borde azul
+</div>
+<?php
+
+include 'footer.php';
+
+?>
